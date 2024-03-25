852e466043c7f997dddad059bef3b6450a1c3bac
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-26 01:56:31 +0200

    adding jobs and auth routers and activiting them in app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 require("express-async-errors");
 const connectDB = require("./db/connect");
+const jobsRouter = require("./routes/jobs");
+const authRouter = require("./routes/auth");
 const express = require("express");
 const app = express();
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use(express.json());
 // extra packages
