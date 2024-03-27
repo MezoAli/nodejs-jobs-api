be654315484a4cc4f131a66e5cf1ab673a466a06
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-28 01:41:20 +0200

    updating error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const register = async (req, res) => {
   const { password } = req.body;
   if (password.length < 6) {
@@ -12,13 +12,16 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("invalid credentils");
+  }
   let user = await User.findOne({ email });
   if (!user) {
     throw new BadRequestError("email is not found, please register first");
   }
   const result = await user.comparePassword(password);
   if (!result) {
-    throw new BadRequestError("wrong password");
+    throw new UnauthenticatedError("wrong password");
   }
 
   const token = user.generateToken();
