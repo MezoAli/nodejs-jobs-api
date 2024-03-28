5d37d0a60865a6cdac5a3f5267390d2c848ba170
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-29 01:27:39 +0200

    adding get all jobs,get single job and add job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,18 @@
 const Job = require("../models/Job");
+const { NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  res.status(200).json({ success: true, msg: "get all jobs" });
+  const userId = req.user.id;
+  const allJobs = await Job.find({ createdBy: userId }).sort("createdAt");
+  res.status(200).json({ success: true, count: allJobs.length, jobs: allJobs });
 };
+
 const getSingleJob = async (req, res) => {
   const id = req.params.id;
   const job = await Job.findById(id).populate("createdBy");
+  if (!job) {
+    throw new NotFoundError("no job with that id");
+  }
   res.status(200).json({ success: true, job });
 };
 const updateJob = async (req, res) => {
@@ -18,7 +25,7 @@ const addJob = async (req, res) => {
   const userId = req.user.id;
   const data = { ...req.body, createdBy: userId };
   const job = await Job.create(data);
-  res.status(201).json({ success: true, msg: "add job", job });
+  res.status(201).json({ success: true, msg: "Job added successfully", job });
 };
 
 module.exports = {
